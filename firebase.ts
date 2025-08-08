798c62b0942cf687667558d6695994003f4ac65a
Zacharyjs23 <zacharyjsmith23@gmail.com>
2025-08-08 00:01:05 -0400

    chore: validate firebase env vars

--- firebase.ts
+++ firebase.ts
@@ -6,6 +6,22 @@ import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/aut
 import { getAnalytics, isSupported, Analytics } from 'firebase/analytics';
 import { Platform } from 'react-native';
 
+const requiredEnvVars = [
+  'EXPO_PUBLIC_FIREBASE_API_KEY',
+  'EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN',
+  'EXPO_PUBLIC_FIREBASE_PROJECT_ID',
+  'EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET',
+  'EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID',
+  'EXPO_PUBLIC_FIREBASE_APP_ID',
+  'EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID',
+];
+
+requiredEnvVars.forEach((name) => {
+  if (!process.env[name]) {
+    throw new Error(`Missing environment variable: ${name}`);
+  }
+});
+
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
